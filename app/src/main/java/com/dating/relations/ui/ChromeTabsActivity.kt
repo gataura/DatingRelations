de9bd1a1729ad4156a40f6f5ce50e2d4f2b9c9ff
a17571621 <k612nn@gmail.com>
2019-08-04 10:20:55 +0300

    Initial commit

---
+++ app/src/main/java/com/dating/relations/ui/ChromeTabsActivity.kt
@@ -0,0 +1,25 @@
+package com.dating.relations.ui
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
+import com.dating.relations.EXTRA_TASK_URL
+import com.dating.relations.R
+
+class ChromeTabsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chrome_tabs)
+
+        val builder = CustomTabsIntent.Builder()
+        builder.setToolbarColor(ContextCompat.getColor(this@ChromeTabsActivity, R.color.colorPrimary))
+        builder.addDefaultShareMenuItem()
+        builder.setShowTitle(false)
+        builder.enableUrlBarHiding()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(intent.getStringExtra(EXTRA_TASK_URL)))
+    }
+}
